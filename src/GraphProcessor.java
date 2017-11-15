182ebc9515c09f4777682ca5046e304fef783d4c
B_Wilhelm <bwilhelm@iastate.edu>
2017-11-14 18:15:54 -0600

    File name changes

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,41 +1,55 @@
-import java.util.ArrayList;
-
 /**
- * 
  * @author Brett Wilhelm
- * @author Zach Johnson
- *
+ * @author Chase Zey
  */
 
-public class GraphProcessor {
-	public GraphProcessor(String graphData) {
-		
-	}
-	
-	public int outDegree(String v) {
-		return 0;
-	}
-	
-	public boolean sameComponent(String u, String v) {
-		return true;
-	}
-	
-	public ArrayList<String> componentVerticies(String v) {
-		return null;
-	}
-	
-	public int largestComponent() {
-		return 0;
-	}
-	
-	public int numComponents() {
-		return 0;
-	}
-	
-	public ArrayList<String> bfsPath(String u, String v) {
-		return null;
-	}
-	
-	//any private helper methods
-}
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
 
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add additional methods and fields)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
+
+import java.util.ArrayList;
+
+
+public class GraphProcessor
+{
+	// other member fields and methods
+
+	// NOTE: graphData should be an absolute file path
+	public GraphProcessor(String graphData)
+	{
+		// TODO
+	}
+
+	public int outDegree(String v)
+	{
+		// TODO
+		
+		return 0;
+	}
+
+	public ArrayList<String> bfsPath(String u, String v)
+	{
+		// TODO
+		
+		return null;
+	}
+
+	public int diameter()
+	{
+		// TODO
+		
+		return 0;
+	}
+
+	public int centrality(String v)
+	{
+		// TODO
+		
+		return 0;
+	}
+}
